83df4a6b9d27cf8285251b7df2edc6fdcf91859f
tdogcodes <trxcycsgo@gmail.com>
2025-05-19 23:51:40 -0700

    auth graphql schema

--- dashboard/src/providers/data/index.tsx
+++ dashboard/src/providers/data/index.tsx
@@ -3,10 +3,10 @@ import graphqlDataProvider, {
     liveProvider as graphqlLiveProvider,
 } from "@refinedev/nestjs-query";
 import { fetchWrapper } from "./fetchWrapper"
-import { Client, createClient } from "graphql-ws";
+import { createClient } from "graphql-ws";
 
 export const API_BASE_URL = "https://api.crm.refine.dev";
-export const API_URL = "http://api.crm.refine.dev";
+export const API_URL = `${API_BASE_URL}/graphql`;
 export const WS_URL = "wss://api.crm.refine.dev/graphql";
 
 export const client = new GraphQLClient(API_URL, {
